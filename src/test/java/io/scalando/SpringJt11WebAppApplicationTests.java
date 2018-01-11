71053a8791cc58cd5ffa41e37e23c91dd49155b5
rodbs <rodrigo.barriuso@gmail.com>
2018-01-11 11:35:10 +0100

    Initial project

---
+++ src/test/java/io/scalando/SpringJt11WebAppApplicationTests.java
@@ -0,0 +1,16 @@
+package io.scalando;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJt11WebAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
